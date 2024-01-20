ea66e015f866018ae31bfe57751ca94974955051
Vital9i <vitalikdoiniak@gmail.com>
2024-01-20 14:03:58 +0300

    02.js

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,16 @@
-// code
+let number1 = Math.floor(Math.random() * 100);
+function examination(number1) {
+    return number1 % 2 === 0;
+  };
+
+  examination(number1);
+
+  if ( examination(number1)) {
+    console.log(number1 + ': это четное число'); 
+  } else { console.log(number1 + ': это нечетное число')}
+
+
+
+
+
+  
