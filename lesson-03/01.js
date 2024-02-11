d648a7553e03bc36363f8e1bb169a01d62144e79
Vital9i <vitalikdoiniak@gmail.com>
2024-02-11 14:08:22 +0300

    03-01js

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,11 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    const newColors = colors.filter((item) => {
+        return item === "черный" || item === "красный" || item === "желтый";
+    });
+    return newColors.join('-')
+}
+console.log(createColorString());
+
+
